5cdff9954e1edee20497f47c1d50af947df6ca83
Erik <er8261@student.uni-lj.si>
2016-11-20 13:34:41 +0000

    added answering/viewing questions

---
+++ scripts/code.js
@@ -0,0 +1,15 @@
+var loggedIn = false;;
+
+//Login window validation
+function loginValidation(){
+	var usernameL = document.getElementById("usernameLogin").value;
+	var passwordL = document.getElementById("passwordLogin").value;
+
+	if(usernameL == "admin" && passwordL == "admin"){
+		loggedIn = true;
+	}
+	
+	if(loggedIn == true){
+		document.getElementById('profile').style.display = 'block';
+	}
+}
