2effd66db78b054d1d1b09f3f7d820f3be333938
Erik <er8261@student.uni-lj.si>
2016-11-26 16:12:24 +0000

    added register data validation, fixed categories and links

--- scripts/code.js
+++ scripts/code.js
@@ -1,11 +1,32 @@
-var loggedIn = false;
+var emailCorrect = false;
+var passwordsCorrect = false;
 
-//Login window validation
-function loginValidation(){
-	var usernameL = document.getElementById("usernameLogin").value;
-	var passwordL = document.getElementById("passwordLogin").value;
-
-	if(usernameL == "admin" && passwordL == "admin"){
-		loggedIn = true;
+function registrationValidation() {
+	var email = document.getElementById("email").value;
+	var pass1 = document.getElementById("passwordRegister1").value;
+	var pass2 = document.getElementById("passwordRegister2").value;
+	
+	if(!validateEmail(email)){
+		document.getElementById("email").style.background = "#B64747";
+	} else {
+		document.getElementById("email").style.background = "none";
+		emailCorrect = true;
 	}
+	
+	if(pass1 != pass2) {
+		document.getElementById("passwordRegister1").style.background = "#B64747";
+		document.getElementById("passwordRegister2").style.background = "#B64747";
+	} else {
+		document.getElementById("passwordRegister1").style.background = "none";
+		document.getElementById("passwordRegister2").style.background = "none";
+		passwordsCorrect = true;
+	}
+	if(emailCorrect && passwordsCorrect){
+		document.getElementById('registerwindow').style.display='none';
+	}
+}
+
+function validateEmail(email) {
+	var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	return re.test(email);
 }
