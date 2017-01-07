8c4a3515be80713726bb20754be69e4cb12ceebb
Erik <er8261@student.uni-lj.si>
2017-01-07 15:55:30 +0000

    working specific question pages without INSERT part

---
+++ django/AskAnswer/AskAnswer/home/static/code.js
@@ -0,0 +1,32 @@
+var emailCorrect = false;
+var passwordsCorrect = false;
+
+function registrationValidation() {
+	var email = document.getElementById("email").value;
+	var pass1 = document.getElementById("passwordRegister1").value;
+	var pass2 = document.getElementById("passwordRegister2").value;
+	
+	if(!validateEmail(email)){
+		document.getElementById("email").style.background = "#B64747";
+	} else {
+		document.getElementById("email").style.background = "none";
+		emailCorrect = true;
+	}
+	
+	if(pass1 != pass2) {
+		document.getElementById("passwordRegister1").style.background = "#B64747";
+		document.getElementById("passwordRegister2").style.background = "#B64747";
+	} else {
+		document.getElementById("passwordRegister1").style.background = "none";
+		document.getElementById("passwordRegister2").style.background = "none";
+		passwordsCorrect = true;
+	}
+	if(emailCorrect && passwordsCorrect){
+		document.getElementById('registerwindow').style.display='none';
+	}
+}
+
+function validateEmail(email) {
+	var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	return re.test(email);
+}
